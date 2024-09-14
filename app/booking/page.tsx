81fe46a3ddaa5d2c0882875cafa1da98cfc8acba
James Washington <jbwashington@gmail.com>
2024-09-14 15:04:46 -0400

    fixed build errors

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,6 +1,6 @@
 import { Contact as ContactSection } from "@/components/layout/contact"
 
-export default function Contact() {
+export default function BookingPage() {
   return (
     <div className="flex flex-col min-h-screen bg-[#f5f1eb]">
         <ContactSection />
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import { About } from "@/components/layout/about"
-import { Contact } from "@/components/layout/contact"
-import { CTA } from "@/components/layout/cta"
-import { Services } from "@/components/layout/services"
+import { About } from "@/components/layout/about";
+import { Contact } from "@/components/layout/contact";
+import { CTA } from "@/components/layout/cta";
+import { Services } from "@/components/layout/services";
 
 export default function Home() {
-  return (
-<>
-      <CTA />
-      <Services />
-      <About />
-      <Contact />
-</>
-  )
+    return (
+        <>
+            <CTA />
+            <Services />
+            <About />
+            <Contact />
+        </>
+    );
 }
