c707c7bb81c3aafc3e91d2f8c992d1b261edb57c
James Washington <jbwashington@gmail.com>
2024-09-17 19:03:25 -0400

    added description to cta, tweaked z-index on video bg divs

--- components/layout/cta.tsx
+++ components/layout/cta.tsx
@@ -3,8 +3,16 @@ import { buttonVariants } from "../ui/button";
 import { cn } from "@/lib/utils";
 export const CTA = () => {
     return (
-        <section className="w-full h-screen">
+        <section 
+        className="w-full h-screen">
             <div className="relative flex items-center justify-center h-screen overflow-hidden">
+                <div
+                    className={cn(
+                        "h-full w-[100vw]",
+                        "bg-transparent bg-beach",
+                        "absolute -z-20"
+                    )}
+                ></div>
                 <video
                     src="/videos/beach.mp4"
                     autoPlay
@@ -12,16 +20,18 @@ export const CTA = () => {
                     muted
                     playsInline
                     className={cn(
-                        "absolute z-0 min-h-full min-w-full object-cover",
-                        "backdrop-brightness-50"
+                        "absolute -z-0 min-h-full min-w-full object-cover",
                     )}
-                ></video>
-                <div className="container px-4 md:px-6 py-auto z-10 max-w-screen-md">
+                >
+                    <source src="/videos/beach.mp4" type="video/mp4" />
+                </video>
+                <div className="container px-4 md:px-6 py-auto z-10 max-w-screen-sm">
                     <div className="flex flex-col space-y-4">
-                        <div className="space-y-2">
-                            <p className="text-8xl font-bold tracking-tighter">
+                        <div className="space-y-6">
+                            <h2 className="text-8xl font-bold tracking-tighter">
                                 You deserve peace and healing.
-                            </p>
+                            </h2>
+                            <p className="font-semibold leading-relaxed text-2xl">Online therapy services for the NYC area and all over New York</p>
                         </div>
                         <div className="space-x-4">
                             <Link
@@ -29,6 +39,7 @@ export const CTA = () => {
                                 className={buttonVariants({
                                     variant: "default",
                                     size: "lg",
+                                    className: "capitalize"
                                 })}
                             >
                                 Book a free consultation
