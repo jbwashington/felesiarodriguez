6669fa958b3585c71e9593d47cf55f325131225d
James Washington <jbwashington@gmail.com>
2024-09-19 15:45:37 -0400

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -34,7 +35,10 @@ export default function RootLayout({
                     enableSystem
                 >
                     <Header />
-                    <div className="flex-grow">{children}</div>
+                    <div className="flex-grow">
+                        {children}
+                        <Analytics />
+                    </div>
                     <SiteFooter />
                     <Toaster />
                     <TailwindIndicator />
