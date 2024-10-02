d51791bcc8efc5aa406f60df57d04c3fceb9a16e
James Washington <jbwashington@gmail.com>
2024-10-01 21:50:21 -0400

    header made blurry and translucent

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -6,23 +6,21 @@ import { RelaxButton } from "./relax-button";
 import { Separator } from "../ui/separator";
 
 export default function Header() {
-  return (
-    <header className="container relative">
-      <div className="flex h-20 items-center justify-between py-6 max-w-screen-lg mx-auto">
-        <MainNav items={siteConfig.mainNav} />
-        <nav className="space-y-2 inline-flex align-middle">
-          <Link
-            href="/contact"
-            className={buttonVariants({
-              variant: "outline",
-              size: "sm",
-              className: "capitalize mr-2",
-            })}
-          >
-            Book a consultation
-          </Link>
-        </nav>
-      </div>
-    </header>
-  );
+    return (
+        <header className="sticky top-0 z-50 w-full border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+            <div className="container flex min-h-fit h-14 md:h-32 max-w-screen-xl mx-auto items-center justify-between">
+                <MainNav items={siteConfig.mainNav} />
+                <Link
+                    href="/contact"
+                    className={buttonVariants({
+                        variant: "outline",
+                        size: "sm",
+                        className: "capitalize mr-4",
+                    })}
+                >
+                    Book a consultation
+                </Link>
+            </div>
+        </header>
+    );
 }
