5664cb609203571870c8768ae1ab5e360e02fd81
James Washington <jbwashington@gmail.com>
2024-11-22 12:50:52 -0500

    video hook removed from cta

--- components/layout/cta.tsx
+++ components/layout/cta.tsx
@@ -4,53 +4,11 @@ import Link from "next/link";
 import { buttonVariants } from "../ui/button";
 import { Icons } from "../ui/icons";
 import { cn } from "@/lib/utils";
-import { useVideoStore } from "@/store/use-video";
-import { useEffect, useRef } from "react";
 
 export const CTA = () => {
-    const video = useVideoStore((state) => state.video);
-
-    console.log(video);
-
-    const videoRef = useRef<HTMLVideoElement>(null);
-
-    useEffect(() => {
-        if (videoRef.current && video) {
-            const source = videoRef.current.querySelector("source");
-            if (source) {
-                source.src = video.url;
-                videoRef.current.load();
-            }
-        }
-    }, [video]);
-
     return (
         <section className="w-full">
             <div className="relative flex items-center justify-center h-screen overflow-hidden -mt-20">
-                {video && (
-                    <>
-                        <div
-                            className={cn(
-                                "h-full w-[100vw]",
-                                "bg-transparent",
-                                "absolute -z-20",
-                                video.slug
-                            )}
-                        ></div>
-                        <video
-                            className="absolute inset-0 object-cover w-full h-full -z-20"
-                            ref={videoRef}
-                            controls
-                            autoPlay
-                            loop
-                            muted
-                            playsInline
-                            disablePictureInPicture
-                        >
-                            <source src={video.url} type="video/mp4" />
-                        </video>
-                    </>
-                )}
                 <div className="px-4 md:px-6 max-w-screen-lg">
                     <div className="flex flex-col space-y-6">
                         <h2
