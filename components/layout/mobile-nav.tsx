628ded285ff272d842b57148d1a8596bec8aab7d
James Washington <jbwashington@gmail.com>
2024-12-19 21:32:59 -0600

    made nav columns instead of one row; made font serif

--- components/layout/mobile-nav.tsx
+++ components/layout/mobile-nav.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 import { MainNavItem } from "@/types";
 import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
-// import { useLockBody } from "@/hooks/use-lock-body";
 import { Icons } from "../ui/icons";
 import { buttonVariants } from "../ui/button";
+import { Separator } from "../ui/separator";
 
 interface MobileNavProps {
     items: MainNavItem[];
@@ -14,34 +14,34 @@ interface MobileNavProps {
 }
 
 export function MobileNav({ items, children }: MobileNavProps) {
-    // useLockBody();
-
     return (
         <div
             className={cn(
-                "fixed inset-0 top-16 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-6 pb-32 shadow-md animate-in slide-in-from-bottom-80 md:hidden"
+                "fixed inset-0 top-16 z-50 grid h-[100vh] grid-flow-row auto-rows-max overflow-auto p-0 pb-32 shadow-md animate-in slide-in-from-bottom-80 md:hidden"
             )}
         >
-            <div className="relative z-20 grid gap-6 rounded-md bg-popover p-4 text-popover-foreground shadow-md align-middle items-center">
-                <Link href="/" className="flex flex-col mx-auto items-center">
-                    <Icons.therapistFelesiaLogoMin className="h-fit w-1/2 py-4" />
-                    <h1 className="font-bold text-lg">{siteConfig.name}</h1>
+            <div className="relative z-20 grid gap-6 rounded-md bg-popover p-4 text-popover-foreground shadow-md items-center justify-center">
+                <Link href="/" className="flex flex-col items-center">
+                    <Icons.therapistFelesiaLogoMin className="h-fit w-1/2 py-8" />
+                    <h1 className="font-bold text-2xl font-serif">{siteConfig.name}</h1>
                 </Link>
-                <nav className="mx-auto">
+                <Separator />
+                <nav className="grid gap-2 w-full">
                     {items.map((item, index) => (
                         <Link
                             key={index}
                             href={item.disabled ? "#" : item.href}
                             className={cn(
-                                "w-full rounded-md p-2 text-sm font-medium hover:underline capitalize",
-                                item.disabled && "cursor-not-allowed opacity-60"
+                                "w-full rounded-md p-2 text-sm font-medium text-center hover:underline capitalize",
+                                item.disabled && "cursor-not-allowed opacity-60",
+                                'font-serif uppercase'
                             )}
                         >
                             {item.title}
                         </Link>
                     ))}
                 </nav>
-                <div className="mx-auto w-full">
+                <div className="w-full">
                     <Link
                         className={buttonVariants({
                             variant: "default",
